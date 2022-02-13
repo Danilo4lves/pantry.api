9d2217baa71abad7719008d35361332732096196
Danilo Alves <danilo.alves@kinvo.com.br>
2022-02-13 17:45:32 -0300

    feat(UsersModule): Imports user entity

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { CREATE_USER_REPOSITORY, USER_REPOSITORY } from './data';
 import {
   FacadeTypeOrmUserRepository,
   TypeOrmCreateUserRepository,
+  UserEntity,
 } from './infra';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserEntity])],
   providers: [
     { provide: CREATE_USER_REPOSITORY, useClass: TypeOrmCreateUserRepository },
     { provide: USER_REPOSITORY, useClass: FacadeTypeOrmUserRepository },
